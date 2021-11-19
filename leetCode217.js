2e0194ae380300f9eb074c5c58ed3f563989c8d4
Justin Sebastian <justinthomassebastian@gmail.com>
2021-11-18 18:22:23 -0700

    first commit

---
+++ leetCode217.js
@@ -0,0 +1,18 @@
+/* Problem: Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+*/
+ 
+ //IN -> number[]
+ //OUT -> boolean   
+let containsDuplicate = function(nums) {
+    
+    let set = new Set();
+    
+    for(let num of nums){
+        if(set.has(num)){
+            return true;
+        } else {
+            set.add(num);
+        }
+    }
+    return false;
+};
