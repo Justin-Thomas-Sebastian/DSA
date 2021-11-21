172695bde00e26a2fa8293fced1dec0a70723e17
Justin Sebastian <justinthomassebastian@gmail.com>
2021-11-20 20:40:37 -0700

    refactor 268

--- leetCode268.js
+++ leetCode268.js
@@ -5,37 +5,32 @@ Problem: Given an array nums containing n distinct numbers in the range [0, n], 
 // IN -> number[]
 // OUT -> number
 
- var missingNumber = function(nums) {
+let missingNumber = function(nums) {
     
 //     SOLUTION USING AN ARRAY
 //     nums.sort( (a, b) => (a - b) ) ;
- 
 //     if(nums[0] !== 0) {
 //         return 0;
 //     }
- 
 //     if(nums[nums.length - 1] !== nums.length) {
 //         return nums.length;
-//     }
-         
- 
+//     } 
 //     for(let i = 0; i < nums.length - 1; i++){
 //        if(nums[i + 1] !== i + 1){
 //            return i + 1;
 //        }    
 //     }
  
-
  // SOLUTION USING MATH
- let actualTotal = 0;
- for(let num of nums){
-     actualTotal += num; 
- }
- 
- let expectedTotal = 0;
- for(let i = 0; i <= nums.length; i++){
-     expectedTotal += i;
- }
- 
- return expectedTotal - actualTotal;
+let actualTotal = 0;
+for(let num of nums){
+    actualTotal += num; 
+}
+
+let expectedTotal = 0;
+for(let i = 0; i <= nums.length; i++){
+    expectedTotal += i;
+}
+
+return expectedTotal - actualTotal;
 };
