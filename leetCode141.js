b3c043c5fd6d97b1d5d562ca135af530d01eb923
Justin Sebastian <justinthomassebastian@gmail.com>
2021-11-24 18:12:59 -0700

    added hash set solution

--- leetCode141.js
+++ leetCode141.js
@@ -10,15 +10,39 @@
 // OUT -> Boolean
 
  let hasCycle = function(head) {
-    let slow = head;
-    let fast = head;
+
+    // USING SLOW AND FAST POINTER 
+    // time -> O(N)
+    // space -> O(1)
+
+    // let slow = head;
+    // let fast = head;
     
-    while(fast && fast.next){
-        slow = slow.next;
-        fast = fast.next.next;
-        if(slow == fast){
+    // while(fast && fast.next){
+    //     slow = slow.next;
+    //     fast = fast.next.next;
+    //     if(slow == fast){
+    //         return true;
+    //     }
+    // }
+    // return false;  
+
+    // USING A HASH SET
+    // time -> O(n)
+    // space -> O(n)
+
+    if(head === null) return false;
+    
+    let set = new Set();
+    
+    while(head.next !== null){
+        
+        if(set.has(head)){
             return true;
+        } else {
+            set.add(head);
+            head = head.next; 
         }
     }
-    return false;  
+    return false;
 };
