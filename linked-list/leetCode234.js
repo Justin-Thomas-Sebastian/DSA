5a69446d5b1252fe73ac4f65a949085db92fee5f
Justin Sebastian <justinthomassebastian@gmail.com>
2021-12-20 20:19:13 -0700

    added 234. added problem description

---
+++ linked-list/leetCode234.js
@@ -0,0 +1,16 @@
+/* Problem: Given the head of a singly linked list, return true if it is a palindrome. */
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+
+// IN -> ListNode
+// OUT -> Boolean
+
+let isPalindrome = function(head) {
+    
+};
