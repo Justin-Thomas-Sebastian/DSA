57f3347dfe0ca9c701c8814a398b448a3048742c
Justin Sebastian <justinthomassebastian@gmail.com>
2021-12-20 21:28:26 -0700

    added 234 solution

--- linked-list/leetCode234.js
+++ linked-list/leetCode234.js
@@ -13,4 +13,42 @@
 
 let isPalindrome = function(head) {
     
+    let fast = head;
+    let slow = head;
+    
+    // fast reaches end of list, slow will be in the middle
+    while(fast !== null && fast.next !== null){
+        fast = fast.next.next;
+        slow = slow.next;
+    }
+    
+    slow = reverseList(slow); //returns list from last to middle. Ex. a b c d e  -> e d c                  
+    fast = head;  //return to first node
+    
+    //compare nodes of original with nodes of reversed
+    while(slow != null){
+        if(slow.val !== fast.val){
+            return false;
+        }
+        slow = slow.next;
+        fast = fast.next;
+    }
+    return true;
+};
+
+// helper function. used to reverse from middle to end of original list
+let reverseList = function(head) {
+    
+    let prev = null;
+    let curr = head;
+    let next = null;
+    
+    while(curr != null){
+        next = curr.next // stash
+        curr.next = prev;  // reverse
+        prev = curr //advance
+        curr = next  //advance
+    }
+    
+    return prev;
 };
