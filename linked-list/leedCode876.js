9d6d0d136e8890b90eeba8c57b09ee800636dcb4
Justin Sebastian <justinthomassebastian@gmail.com>
2021-12-21 17:54:23 -0700

    added 876 problem description

---
+++ linked-list/leedCode876.js
@@ -0,0 +1,16 @@
+/* 
+Problems: Given the head of a singly linked list, return the middle node of the linked list.
+If there are two middle nodes, return the second middle node.
+*/
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+
+// IN -> ListNode
+// OUT -> ListNode
+
