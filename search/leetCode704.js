e4c514c1e4df0017f69ad253e035578cfb38da66
Justin Sebastian <justinthomassebastian@gmail.com>
2021-12-24 19:45:28 -0700

    added 704 problem description

---
+++ search/leetCode704.js
@@ -0,0 +1,10 @@
+/*
+Problem: Given an array of integers nums which is sorted in ascending order and an integer target.
+Write a function to search target in nums. 
+If target exists, then return its index. 
+Otherwise, return -1.
+You must write an algorithm with O(log n) runtime complexity.
+ */
+
+// IN -> number[], number(target)
+// OUT -> number(index)
