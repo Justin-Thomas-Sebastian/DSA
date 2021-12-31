744c6372b8d19e09bed594972e8c9266b6058ca8
Justin Sebastian <justinthomassebastian@gmail.com>
2021-12-30 23:15:18 -0700

    added leetcode1 two-pointer solution

--- two-pointers/leetCode1.js
+++ two-pointers/leetCode1.js
@@ -8,3 +8,39 @@ and you may not use the same element twice.
 
 //IN -> Number[]
 //OUT -> Number[]
+
+let twoSum = function(nums, target) {
+	let copy = [...nums];
+    copy.sort( (a, b) => (a - b) ) // sort in ascending
+    
+	let left = 0;
+	let right = copy.length - 1;
+    let result = [];
+	
+	while(left < right){
+		let currentSum = copy[left] + copy[right];
+		if(currentSum === target){
+			break;
+		} else if(currentSum < target){
+			left++;
+		} else {
+			right--;
+		}
+	}
+	
+    for(let i = 0; i < nums.length; i++){ // find left index in original array
+        if(nums[i] === copy[left]){
+            result.push(i);
+            break;
+        }
+    }
+    
+    for(let i = nums.length - 1; i >= 0; i--){ // find right index in original array
+        if(nums[i] === copy[right]){
+            result.push(i);
+            break;
+        }
+    }
+    
+    return result;
+}    
