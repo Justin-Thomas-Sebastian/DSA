a6e6dd13a9da4861a65d2db92cc9a2e26e3de693
Justin Sebastian <justinthomassebastian@gmail.com>
2022-01-02 11:55:26 -0600

    added 977 solution

--- two-pointers/leetCode977.js
+++ two-pointers/leetCode977.js
@@ -5,3 +5,20 @@
  Follow up: Squaring each element and sorting the new array is very trivial, 
  could you find an O(n) solution using a different approach?
  */
+
+ let sortedSquares = function(nums) {    
+    let result = [];
+    let left = 0;
+    let right = nums.length - 1;
+    
+    while(left <= right){
+        if(Math.abs(nums[left]) >= Math.abs(nums[right])){
+            result.push(nums[left] ** 2);
+            left++;
+        } else {
+            result.push(nums[right] ** 2);
+            right--;
+        }
+    }
+    return result.reverse();
+};
