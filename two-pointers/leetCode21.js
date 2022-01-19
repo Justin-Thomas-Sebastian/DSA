0e9f5023a550b748b4275578167d40562fc954fb
Justin Sebastian <justinthomassebastian@gmail.com>
2022-01-18 22:46:13 -0700

    added leetcode 21 problem description

---
+++ two-pointers/leetCode21.js
@@ -0,0 +1,22 @@
+/*
+Problem: You are given the heads of two sorted linked lists list1 and list2.
+
+Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+Return the head of the merged linked list.
+*/
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+
+// IN -> ListNode1, ListNode2
+// OUT -> ListNode
+
+let mergeTwoLists = function(list1, list2) {
+    
+};
